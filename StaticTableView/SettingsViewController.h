d15e8e7f640beb6197ceaee9314150156783cb88
Tue <tueht.ever@gmail.com>
2014-03-11 15:58:08 +0700

    First commit

---
+++ StaticTableView/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  StaticTableView
+//
+//  Created by Tue Hoang Trong on 3/11/14.
+//  Copyright (c) 2014 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+// Handle all switch change press
+- (IBAction)touchChange:(id)sender;
+
+@end
---
+++ StaticTableView/SettingsViewController.m
@@ -0,0 +1,151 @@
+//
+//  SettingsViewController.m
+//  StaticTableView
+//
+//  Created by Tue Hoang Trong on 3/11/14.
+//  Copyright (c) 2014 demo. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+//
+//#pragma mark - Table view data source
+//
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+//#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 0;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//#warning Incomplete method implementation.
+//    // Return the number of rows in the section.
+//    return 0;
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *CellIdentifier = @"Cell";
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+//    
+//    // Configure the cell...
+//    
+//    return cell;
+//}
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+- (IBAction)touchChange:(id)sender {
+    NSLog(@"change with tag: %d", [sender tag]);
+    UISwitch *onOff = (UISwitch*)sender;
+    NSString *control = nil;
+    
+    switch ([sender tag]) {
+        case 101:
+            control = @"mobile notification";
+            break;
+        case 102:
+            control = @"email notification";
+            break;
+        case 103:
+            control = @"Calendar sync";
+            break;
+        case 104:
+            control = @"Dropbox sync";
+            break;
+        case 105:
+            control = @"Google Drive sync";
+            break;
+        default:
+            break;
+    }
+    
+    
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Setting changed!" message:[NSString stringWithFormat:@"You changed setting of %@ to %@", control, onOff.on ? @"On" : @"Off"]
+                          delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    
+    [alert show];
+}
+@end
